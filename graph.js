541452d5485f11768a5b9a22e85cf6a4280e70bd
Toni Alatalo <toni.alatalo@gmail.com>
2022-09-12 15:44:14 +0300

    month names match hack. disable unused compare section in html

--- graph.js
+++ graph.js
@@ -67,6 +67,25 @@ function drawGraph(data) {
 function drawBarChart(data) {
   const y = graphSetup(data);
 
+  //match hack
+  /* tr 40
+  22 -> 40, 1.07
+  10 -> 60, 1.13
+   6 -> 100, 1.25 # 
+   8 -> 1.2
+
+  >>> w = lambda n: 1 + (1.5/n)
+  >>> w(6)
+  1.25
+  >>> w(10)
+  1.15
+  >>> w(22)
+  1.0681818181818181
+  */
+  let n = data.length;
+  tr_x = 140 - (n * 7);
+  wdiv = 1 + (1.7 / n)
+
   const xBand = d3.scaleBand()
     .range([ 0, width ])
     .domain(data.map(function(d) { return d.date; }))
@@ -74,14 +93,14 @@ function drawBarChart(data) {
 
   //tried to get the month names to x axis legend also for bars but failed so far - got month names, but then wrong bar widths / positioning
   var x = d3.scaleTime()
-    .domain(d3.extent(data, function(d) { return d.date; }))
-    .range([ 0, width ]);
+  .range([ 0, width / wdiv])
+  .domain(d3.extent(data, function(d) { return d.date; }));
 
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
     .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
+    .attr("transform", "translate(" + tr_x + ",10)rotate(0)")
     .style("text-anchor", "end");
 
   /*
